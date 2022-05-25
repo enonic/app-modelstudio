4d4e6f687a5a5b1a1b979d75cfaf1d453075c45f
Bradnitski <vbr@enonic.com>
2022-05-25 14:58:32 +0300

    Cleanup code from users-app legacy #13 phrases

--- src/main/resources/assets/js/graphql/schema/ListSchemasRequest.ts
+++ src/main/resources/assets/js/graphql/schema/ListSchemasRequest.ts
@@ -50,8 +50,7 @@ export class ListSchemasRequest
                         description,
                         displayName,
                         resource,
-                        type,
-                        icon
+                        type
                     }
                 }`;
     }
